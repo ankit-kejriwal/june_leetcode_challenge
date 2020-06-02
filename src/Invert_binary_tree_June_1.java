21076c7f4e458a6ef66d9cd2e389a26190b0bca1
ankit <ankitkejriwaal@gmail.com>
2020-06-01 23:07:16 -0400

    Initial commit

---
+++ src/Invert_binary_tree_June_1.java
@@ -0,0 +1,19 @@
+
+public class Invert_binary_tree_June_1 {
+
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+
+    public void helper(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        helper(root.left);
+        helper(root.right);
+    }
+}
