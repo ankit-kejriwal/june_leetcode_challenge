98a8efbe8db3d71b4598062417ff7dfca6f85bed
ankit <ankitkejriwaal@gmail.com>
2020-06-01 23:19:41 -0400

    first day challenge

--- src/Invert_binary_tree_June_1.java
+++ src/Invert_binary_tree_June_1.java
@@ -1,6 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Invert_binary_tree_June_1 {
-
+    TreeNode root;
     public TreeNode invertTree(TreeNode root) {
         helper(root);
         return root;
@@ -15,5 +17,36 @@ public class Invert_binary_tree_June_1 {
         root.right = temp;
         helper(root.left);
         helper(root.right);
+    }
+    public void createSampleTree(){
+        root = new TreeNode(4,new TreeNode(2,new TreeNode(1),new TreeNode(3)),new TreeNode(6,new TreeNode(5),new TreeNode(7)));
+    }
+
+    public void levelOrderTraversal(TreeNode temp){
+        if(temp == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(temp);
+        while (!queue.isEmpty()){
+            TreeNode curr = queue.poll();
+            System.out.print(curr.val+ " ");
+            if(curr.left !=null){
+                queue.add(curr.left);
+            }
+            if(curr.right !=null){
+                queue.add(curr.right);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Invert_binary_tree_June_1 invertTree = new Invert_binary_tree_June_1();
+        invertTree.createSampleTree();
+        invertTree.levelOrderTraversal(invertTree.root);
+        invertTree.invertTree(invertTree.root);
+        System.out.println();
+        invertTree.levelOrderTraversal(invertTree.root);
     }
 }
